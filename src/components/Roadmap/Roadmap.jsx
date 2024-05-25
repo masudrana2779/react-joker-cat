1a1b526c26bc7a54ac19e3c1aeda011e386ae9d8
Masud Rana <masudrana2779@gmail.com>
2024-05-25 21:41:55 +0600

    i hope responsive done

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -18,31 +18,46 @@ const Roadmap = () => {
       title: "Phase one",
       listItems: [
         { list: "Launch on Raydium", icon: "/img/checked.svg" },
-        { list: "Marketing + Community Building", icon: "/img/checked.svg" },
-        { list: "PinkSale AMA", icon: "/img/checked.svg" },
-        { list: "TG & Twitter Raids", icon: "/img/checked.svg" },
-        { list: "Private Call Group Features", icon: "/img/checked.svg" },
-        { list: "Pinksale Fair Launch", icon: "/img/checked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/checked.svg" },
+        { list: "CMC / CG Fast Track Listings", icon: "/img/checked.svg" },
+        { list: "Go live on Jupiter", icon: "/img/checked.svg" },
+        { list: "Trust Wallet Integration", icon: "/img/checked.svg" },
+        { list: "Listing on MEXC", icon: "/img/checked.svg" },
+        { list: "Listing on BitMart", icon: "/img/checked.svg" },
+        { list: "News/Blog on the website", icon: "/img/checked.svg" },
       ],
     },
     {
       title: "Phase one",
       listItems: [
-        { list: "Website & Socials Launch", icon: "/img/checked.svg" },
-        { list: "Marketing + Community Building", icon: "/img/checked.svg" },
-        { list: "PinkSale AMA", icon: "/img/checked.svg" },
-        { list: "TG & Twitter Raids", icon: "/img/checked.svg" },
-        { list: "Private Call Group Features", icon: "/img/checked.svg" },
-        { list: "Pinksale Fair Launch", icon: "/img/checked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
-        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        {
+          list: "Launch Online Crypto Casino Partnership",
+          icon: "/img/checked.svg",
+        },
+        {
+          list: "Multi language website inc. Chinese",
+          icon: "/img/checked.svg",
+        },
+        {
+          list: "Launch Strategic Partnership w/GotBit Hedge Fund",
+          icon: "/img/checked.svg",
+        },
+        {
+          list: "Launch Strategic Partnership with The Gari Network / Chingari App",
+          icon: "/img/checked.svg",
+        },
+        { list: "Launch $CATNIP incentive token", icon: "/img/checked.svg" },
+        {
+          list: "Launch Joker cat DAPP (including staking+farming+fiat onramp)",
+          icon: "/img/checked.svg",
+        },
+        { list: "Unveil NFT Collection", icon: "/img/unchecked.svg" },
+        { list: "Mint first 200 NFTs", icon: "/img/unchecked.svg" },
+        { list: "Reach $100m Market Cap", icon: "/img/unchecked.svg" },
+        { list: "Listing on Tier 1 Exchange", icon: "/img/unchecked.svg" },
+        {
+          list: "Become the first cat token to reach $1B Market Cap",
+          icon: "/img/unchecked.svg",
+        },
       ],
     },
   ];
@@ -53,7 +68,7 @@ const Roadmap = () => {
           <div className="text-[28px] leading-[44px] md:text-[42px] md:leading-[58px] text-white mb-[60px] acostaregular text-center">
             Roadmap
           </div>
-          <div className="container grid grid-cols-1 md:grid-cols-3 gap-[50px] md:gap-[110px]">
+          <div className="container grid grid-cols-1 md:grid-cols-3 gap-[50px] md:gap-[50px]">
             {roadmapData.map((item, i) => (
               <div key={i} className="">
                 <div className="text-white acostaregular text-[22px] leading-[28px] md:text-[30px] md:leading-[38px] mb-[20px] md:mb-[30px]">
@@ -63,7 +78,7 @@ const Roadmap = () => {
                   {item.listItems.map((listItem, l) => (
                     <li key={l} className="mb-[10px]">
                       <div className="flex items-center">
-                        <div className="mr-4">
+                        <div className="mr-4 min-w-[21px]">
                           <img src={listItem.icon} alt="img" />
                         </div>
                         <div className="roboto font-semibold text-white text-base leading-6">
