7646b2d87cadbf2e1d61ef79b7812a1a505dd872
Masud Rana <masudrana2779@gmail.com>
2024-05-25 19:53:59 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
+import { FaTelegramPlane } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-[#140933] py-10">
-      <div className="max-w-[1170px] mx-auto flex justify-between px-[15px]">
+    <div className="bg-[#140933] pt-[100px] pb-10">
+      <div className="max-w-[1170px] mx-auto flex justify-between px-[15px] w-full mb-16">
         <div className="w-full max-w-[256px]">
           <div className=" flex items-center space-x-4 mb-5">
             <div className="">
@@ -51,21 +53,76 @@ const Footer = () => {
           <div className="">
             <ul>
               <li>
-                <Link
+                <a
                   target="_black"
-                  to={"/"}
-                  className="flex items-center space-x-4"
+                  href={"/"}
+                  className="flex items-center space-x-2"
                 >
-                  <div className="">
+                  <div className="text-[#DC3C4D]">
                     <FaXTwitter />
                   </div>
-                  <div className="">@JokerJcat</div>
-                </Link>
+                  <div className="text-base text-[#54595F] underline">
+                    @JokerJcat
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_black"
+                  href={"/"}
+                  className="flex items-center space-x-2"
+                >
+                  <div className="text-[#DC3C4D]">
+                    <FaTelegramPlane />
+                  </div>
+                  <div className="text-base text-[#54595F] underline">
+                    @JokerJcat
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_black"
+                  href={"/"}
+                  className="flex items-center space-x-2"
+                >
+                  <div className="text-[#DC3C4D]">
+                    <MdAlternateEmail />
+                  </div>
+                  <div className="text-base text-[#54595F] underline">
+                    info@jokercat.club
+                  </div>
+                </a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="w-full max-w-[391px]">sd</div>
+        <div className="w-full max-w-[391px]">
+          <div className="text-white text-[25px] leading-[25px] font-bold mb-[22px]">
+            Join Jcat Academy
+          </div>
+          <div className="text-base text-white mb-5">
+            Sign up for our newsletter to keep updated about Joker Cat.
+          </div>
+          <div className="">
+            <form action="#">
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="text-base text-white bg-transparent border-b border-[#E95455] outline-none shadow-none w-full mb-5"
+              />
+              <button
+                type="submit"
+                className="border border-[#E23125] rounded-full text-[#E23125] py-[10px] px-[20px] hover:bg-[#E23125] hover:text-white transition-all duration-200 text-base leading-4"
+              >
+                Sign Me Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="w-full max-w-[1170px] mx-auto px-[15px] text-white text-base">
+        <div className="">© 2024 Joker Cat. All Rights Reserved.</div>
       </div>
     </div>
   );
