4ab92efadb10d3d27a155d7f3e6c2adcd2907457
Masud Rana <masudrana2779@gmail.com>
2024-05-25 16:19:51 +0600

    roadmap done with demo data

---
+++ src/components/Roadmap/Roadmap.jsx
@@ -0,0 +1,81 @@
+const Roadmap = () => {
+  const roadmapData = [
+    {
+      title: "Phase one",
+      listItems: [
+        { list: "Website & Socials Launch", icon: "/img/checked.svg" },
+        { list: "Marketing + Community Building", icon: "/img/checked.svg" },
+        { list: "PinkSale AMA", icon: "/img/checked.svg" },
+        { list: "TG & Twitter Raids", icon: "/img/checked.svg" },
+        { list: "Private Call Group Features", icon: "/img/checked.svg" },
+        { list: "Pinksale Fair Launch", icon: "/img/checked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/checked.svg" },
+      ],
+    },
+    {
+      title: "Phase one",
+      listItems: [
+        { list: "Launch on Raydium", icon: "/img/checked.svg" },
+        { list: "Marketing + Community Building", icon: "/img/checked.svg" },
+        { list: "PinkSale AMA", icon: "/img/checked.svg" },
+        { list: "TG & Twitter Raids", icon: "/img/checked.svg" },
+        { list: "Private Call Group Features", icon: "/img/checked.svg" },
+        { list: "Pinksale Fair Launch", icon: "/img/checked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/checked.svg" },
+      ],
+    },
+    {
+      title: "Phase one",
+      listItems: [
+        { list: "Website & Socials Launch", icon: "/img/checked.svg" },
+        { list: "Marketing + Community Building", icon: "/img/checked.svg" },
+        { list: "PinkSale AMA", icon: "/img/checked.svg" },
+        { list: "TG & Twitter Raids", icon: "/img/checked.svg" },
+        { list: "Private Call Group Features", icon: "/img/checked.svg" },
+        { list: "Pinksale Fair Launch", icon: "/img/checked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+        { list: "Dexscreener + Dexview Trending", icon: "/img/unchecked.svg" },
+      ],
+    },
+  ];
+  return (
+    <div>
+      <div className="pt-[100px] pb-[130px]">
+        <div className="text-[42px] leading-[58px] text-white mb-[60px] acostaregular text-center">
+          Roadmap
+        </div>
+        <div className="container grid grid-cols-3 gap-[110px]">
+          {roadmapData.map((item, i) => (
+            <div key={i} className="">
+              <div className="text-white acostaregular text-[30px] leading-[38px] mb-[30px]">
+                {item.title}
+              </div>
+              <ul>
+                {item.listItems.map((listItem, l) => (
+                  <li key={l} className="mb-[10px]">
+                    <div className="flex items-center">
+                      <div className="mr-4">
+                        <img src={listItem.icon} alt="img" />
+                      </div>
+                      <div className="roboto text-white text-base leading-6">
+                        {listItem.list}
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Roadmap;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
 import Banner from "../../Banner/Banner";
 import { JokerCatAcademy } from "../../JokerCatAcademy/JokerCatAcademy";
 import Nav from "../../Nav/Nav";
+import Roadmap from "../../Roadmap/Roadmap";
 
 const Home = () => {
   return (
-    <>
-      <div className="bgColor pb-[100px]">
+    <div className="bgColor">
+      <div className="pb-[100px]">
         <Nav />
         <Banner />
       </div>
       <JokerCatAcademy />
-    </>
+      <Roadmap />
+    </div>
   );
 };
 
